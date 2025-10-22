031ccd56774ff6c7e714d54e32bcd191b2389466
Juan Butron <yosoyunclap@gmail.com>
2025-10-22 09:57:50 -0400

    Refactorizacion del diseño de registrarse como nuevo usaurio

--- lib/screens/iniciar_secion.dart
+++ lib/screens/iniciar_secion.dart
@@ -117,7 +117,7 @@ class _ScreenIniciarSecionState extends State<ScreenIniciarSecion> {
         padding: const EdgeInsets.only(top: 30),
         child: Column(
           children: [
-            Image.asset("assets/images/logo_YACHAY.png", height: 240),
+            Image.asset("assets/images/logoV1.png", height: 240),
 
             const SizedBox(height: 6),
             Text(
